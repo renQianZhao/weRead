c610d117db81082d9e6cf3cf5694918cd2c37a41
zhaoRen <renzhao1016@163.com>
2018-11-20 23:33:21 +0800

    增加按钮图标

--- src/components/books/booksPage.vue
+++ src/components/books/booksPage.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="wrap">
-        <div id="test">books</div>
+        <div id="test">
+            <img width="100%" height="100%" src="http://01imgmini.eastday.com/mobile/20180920/20180920152643_4975a18ad7e7150c7506fe45fbb581a7_3.jpeg">
+        </div>
+        
     </div>
 </template>
 <script>
--- src/components/found/foundPage.vue
+++ src/components/found/foundPage.vue
@@ -1,17 +1,121 @@
 <template>
     <div id="wrap">
-        <div id="test">found</div>
+        <div id="search">
+            <div id="searchIcon">
+                <img width="100%" src="../../assets/img/search_icon.png"/>
+            </div>
+            <el-input type="primery" class="searchInput" v-model="searchKeyWord" clearable placeholder="情书"></el-input>
+            <span id="searchTitle">搜索</span>
+        </div>
+        <div id="news"></div>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            searchKeyWord: ''
+        }
+    }
 }
 </script>
 <style scoped>
-#test {
+#wrap {
     width: 100%;
     height: 100%;
     background-color: rgb(196, 56, 56);
+}
+#search {
+    margin: 10px auto;
+    padding: 0;
+    width: 85%;
+    height: 40px;
+    background-color: #ffffff;
+    border-top-left-radius: 20px 50%;
+    border-bottom-left-radius: 20px 50%;
+    border-top-right-radius: 20px 50%;
+    border-bottom-right-radius: 20px 50%;
+}
+#searchIcon {
+    position: relative;
+    display: inline-block;
+    width: 15%;
+    height: 40px;
+}
+#searchIcon img {
+    display: inline-table;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    margin-top: -12.5px;
+    width: 25px;
+    height: 25px;
+}
+.searchInput {
+    width: 65%;
+    top: -15px;
+}
+.searchInput::after {
+    position: absolute;
+    top: 50%;
+    margin-top: -7px;
+    width: 1px;
+    height: 14px;
+    content: "";
+    background-color: #dcdfe6;
+}
+.searchInput >>> .el-input__inner {
+    padding: 0 5px;
+    border: 0px solid #dcdfe6;
+    border-radius: 0px;
+}
+#searchTitle {
+    position: relative;
+    display: inline-block;
+    padding-left: 10px; 
+    top: -15px;
+    width: 10%;
+    font-size: 14px;
+    line-height: 40px;
+    text-align: center;
+    color: #606266;
+}
+#news {
+    margin: auto;
+    width: 85%;
+    height: 550px;
+    border-radius: 8px;
+    background-color: #000;
+}
+@media screen and (max-width: 320px){
+    #search {
+        height: 30px;
+    }
+    #searchIcon {
+        width: 12%;
+        height: 30px;
+    }
+    #searchIcon img {
+        margin-top: -10px;
+        width: 20px;
+        height: 20px;
+    }
+    .searchInput {
+        width: 65%;
+        top: -10px;
+    }
+    .searchInput >>> .el-input__inner {
+        height: 30px;
+        line-height: 30px;
+    }
+    #searchTitle {
+        top: -16px;
+        font-size: 12px;
+    }
+    #searchTitle {
+        top: -11px;
+        font-size: 12px;
+        line-height: 30px;
+    }
 }
 </style>
